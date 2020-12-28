1b839ef7053c722ff71dedc474ea59897395644f
KhadijaNoor777 <khadijanoor1405@gmail.com>
2020-12-28 18:32:05 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
+
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [resourceType, setResourceType] = useState('posts');
+  // const [items, setItems] = useState([]);
+
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // }
+
+  //this useEffect will be called whenver the resource type changes
+  //this return executes first, it can be used to clean up any previous effects before running useEffect 
+  useEffect(() => {
+    console.log('render');
+
+    return () => {
+      console.log('return from resource change')
+    }
+
+    // fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    //   .then(response => response.json())
+    //   .then(json => setItems(json))
+
+    
+  }, [resourceType])
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize)
+  // })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <div>{windowWidth}</div> */}
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+
+      <h2> {resourceType} </h2>
+
+      {/* {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })} */}
+
     </div>
   );
 }
